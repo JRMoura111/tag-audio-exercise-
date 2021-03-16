a8fa9e002091ba722228666c9dd7e1a6a75bb5f8
antonio marcos <junior-moura541@hotmail.com>
2021-03-16 15:08:53 -0300

    exercicio html / Js audio

---
+++ script.js
@@ -0,0 +1,28 @@
+let audio = document.getElementById("audioss");
+
+
+audio.loop();
+
+function pausar(){
+
+    audio.pause();
+
+   
+}
+
+function iniciar(){
+    audio.play();
+
+}
+
+function volume() {
+
+    audio.volume = 1;
+
+    alert("não sei fazer aquela barrinha \nno botão do volume ksksksksk");
+  
+    
+
+
+    
+}
